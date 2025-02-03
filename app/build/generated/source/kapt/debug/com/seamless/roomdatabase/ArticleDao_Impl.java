35554ce8a8563b983716204c051445d79115ff6b
developersuhail <92009885+developersuhail@users.noreply.github.com>
2025-02-03 23:13:26 +0530

    Add files via upload

---
+++ app/build/generated/source/kapt/debug/com/seamless/roomdatabase/ArticleDao_Impl.java
@@ -0,0 +1,223 @@
+package com.seamless.roomdatabase;
+
+import android.database.Cursor;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.lifecycle.LiveData;
+import androidx.room.CoroutinesRoom;
+import androidx.room.EntityInsertionAdapter;
+import androidx.room.RoomDatabase;
+import androidx.room.RoomSQLiteQuery;
+import androidx.room.SharedSQLiteStatement;
+import androidx.room.util.CursorUtil;
+import androidx.room.util.DBUtil;
+import androidx.sqlite.db.SupportSQLiteStatement;
+import java.lang.Class;
+import java.lang.Exception;
+import java.lang.Object;
+import java.lang.Override;
+import java.lang.String;
+import java.lang.SuppressWarnings;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.Callable;
+import kotlin.Unit;
+import kotlin.coroutines.Continuation;
+
+@SuppressWarnings({"unchecked", "deprecation"})
+public final class ArticleDao_Impl implements ArticleDao {
+  private final RoomDatabase __db;
+
+  private final EntityInsertionAdapter<ArticleEntity> __insertionAdapterOfArticleEntity;
+
+  private final SharedSQLiteStatement __preparedStmtOfClearArticles;
+
+  public ArticleDao_Impl(@NonNull final RoomDatabase __db) {
+    this.__db = __db;
+    this.__insertionAdapterOfArticleEntity = new EntityInsertionAdapter<ArticleEntity>(__db) {
+      @Override
+      @NonNull
+      protected String createQuery() {
+        return "INSERT OR REPLACE INTO `articles` (`id`,`author`,`title`,`description`,`url`,`urlToImage`,`publishedAt`,`content`) VALUES (nullif(?, 0),?,?,?,?,?,?,?)";
+      }
+
+      @Override
+      protected void bind(@NonNull final SupportSQLiteStatement statement,
+          @NonNull final ArticleEntity entity) {
+        statement.bindLong(1, entity.getId());
+        if (entity.getAuthor() == null) {
+          statement.bindNull(2);
+        } else {
+          statement.bindString(2, entity.getAuthor());
+        }
+        if (entity.getTitle() == null) {
+          statement.bindNull(3);
+        } else {
+          statement.bindString(3, entity.getTitle());
+        }
+        if (entity.getDescription() == null) {
+          statement.bindNull(4);
+        } else {
+          statement.bindString(4, entity.getDescription());
+        }
+        if (entity.getUrl() == null) {
+          statement.bindNull(5);
+        } else {
+          statement.bindString(5, entity.getUrl());
+        }
+        if (entity.getUrlToImage() == null) {
+          statement.bindNull(6);
+        } else {
+          statement.bindString(6, entity.getUrlToImage());
+        }
+        if (entity.getPublishedAt() == null) {
+          statement.bindNull(7);
+        } else {
+          statement.bindString(7, entity.getPublishedAt());
+        }
+        if (entity.getContent() == null) {
+          statement.bindNull(8);
+        } else {
+          statement.bindString(8, entity.getContent());
+        }
+      }
+    };
+    this.__preparedStmtOfClearArticles = new SharedSQLiteStatement(__db) {
+      @Override
+      @NonNull
+      public String createQuery() {
+        final String _query = "DELETE FROM articles";
+        return _query;
+      }
+    };
+  }
+
+  @Override
+  public Object insertArticles(final List<ArticleEntity> articles,
+      final Continuation<? super Unit> $completion) {
+    return CoroutinesRoom.execute(__db, true, new Callable<Unit>() {
+      @Override
+      @NonNull
+      public Unit call() throws Exception {
+        __db.beginTransaction();
+        try {
+          __insertionAdapterOfArticleEntity.insert(articles);
+          __db.setTransactionSuccessful();
+          return Unit.INSTANCE;
+        } finally {
+          __db.endTransaction();
+        }
+      }
+    }, $completion);
+  }
+
+  @Override
+  public Object clearArticles(final Continuation<? super Unit> $completion) {
+    return CoroutinesRoom.execute(__db, true, new Callable<Unit>() {
+      @Override
+      @NonNull
+      public Unit call() throws Exception {
+        final SupportSQLiteStatement _stmt = __preparedStmtOfClearArticles.acquire();
+        try {
+          __db.beginTransaction();
+          try {
+            _stmt.executeUpdateDelete();
+            __db.setTransactionSuccessful();
+            return Unit.INSTANCE;
+          } finally {
+            __db.endTransaction();
+          }
+        } finally {
+          __preparedStmtOfClearArticles.release(_stmt);
+        }
+      }
+    }, $completion);
+  }
+
+  @Override
+  public LiveData<List<ArticleEntity>> getAllArticles() {
+    final String _sql = "SELECT * FROM articles ORDER BY publishedAt DESC";
+    final RoomSQLiteQuery _statement = RoomSQLiteQuery.acquire(_sql, 0);
+    return __db.getInvalidationTracker().createLiveData(new String[] {"articles"}, false, new Callable<List<ArticleEntity>>() {
+      @Override
+      @Nullable
+      public List<ArticleEntity> call() throws Exception {
+        final Cursor _cursor = DBUtil.query(__db, _statement, false, null);
+        try {
+          final int _cursorIndexOfId = CursorUtil.getColumnIndexOrThrow(_cursor, "id");
+          final int _cursorIndexOfAuthor = CursorUtil.getColumnIndexOrThrow(_cursor, "author");
+          final int _cursorIndexOfTitle = CursorUtil.getColumnIndexOrThrow(_cursor, "title");
+          final int _cursorIndexOfDescription = CursorUtil.getColumnIndexOrThrow(_cursor, "description");
+          final int _cursorIndexOfUrl = CursorUtil.getColumnIndexOrThrow(_cursor, "url");
+          final int _cursorIndexOfUrlToImage = CursorUtil.getColumnIndexOrThrow(_cursor, "urlToImage");
+          final int _cursorIndexOfPublishedAt = CursorUtil.getColumnIndexOrThrow(_cursor, "publishedAt");
+          final int _cursorIndexOfContent = CursorUtil.getColumnIndexOrThrow(_cursor, "content");
+          final List<ArticleEntity> _result = new ArrayList<ArticleEntity>(_cursor.getCount());
+          while (_cursor.moveToNext()) {
+            final ArticleEntity _item;
+            final int _tmpId;
+            _tmpId = _cursor.getInt(_cursorIndexOfId);
+            final String _tmpAuthor;
+            if (_cursor.isNull(_cursorIndexOfAuthor)) {
+              _tmpAuthor = null;
+            } else {
+              _tmpAuthor = _cursor.getString(_cursorIndexOfAuthor);
+            }
+            final String _tmpTitle;
+            if (_cursor.isNull(_cursorIndexOfTitle)) {
+              _tmpTitle = null;
+            } else {
+              _tmpTitle = _cursor.getString(_cursorIndexOfTitle);
+            }
+            final String _tmpDescription;
+            if (_cursor.isNull(_cursorIndexOfDescription)) {
+              _tmpDescription = null;
+            } else {
+              _tmpDescription = _cursor.getString(_cursorIndexOfDescription);
+            }
+            final String _tmpUrl;
+            if (_cursor.isNull(_cursorIndexOfUrl)) {
+              _tmpUrl = null;
+            } else {
+              _tmpUrl = _cursor.getString(_cursorIndexOfUrl);
+            }
+            final String _tmpUrlToImage;
+            if (_cursor.isNull(_cursorIndexOfUrlToImage)) {
+              _tmpUrlToImage = null;
+            } else {
+              _tmpUrlToImage = _cursor.getString(_cursorIndexOfUrlToImage);
+            }
+            final String _tmpPublishedAt;
+            if (_cursor.isNull(_cursorIndexOfPublishedAt)) {
+              _tmpPublishedAt = null;
+            } else {
+              _tmpPublishedAt = _cursor.getString(_cursorIndexOfPublishedAt);
+            }
+            final String _tmpContent;
+            if (_cursor.isNull(_cursorIndexOfContent)) {
+              _tmpContent = null;
+            } else {
+              _tmpContent = _cursor.getString(_cursorIndexOfContent);
+            }
+            _item = new ArticleEntity(_tmpId,_tmpAuthor,_tmpTitle,_tmpDescription,_tmpUrl,_tmpUrlToImage,_tmpPublishedAt,_tmpContent);
+            _result.add(_item);
+          }
+          return _result;
+        } finally {
+          _cursor.close();
+        }
+      }
+
+      @Override
+      protected void finalize() {
+        _statement.release();
+      }
+    });
+  }
+
+  @NonNull
+  public static List<Class<?>> getRequiredConverters() {
+    return Collections.emptyList();
+  }
+}
